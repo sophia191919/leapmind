34646496cd7d05fa664bd3c174690fe21fc73689
tumoshaonian <2429534925@qq.com>
2026-07-26 23:19:40 +0800

    fix

--- aitutor-frontend/src/components/practice/index.js
+++
@@ -1,4 +0,0 @@
-export { default as QuestionCard } from "./QuestionCard";
-export { default as ProgressBar } from "./ProgressBar";
-export { default as Timer } from "./Timer";
-export { default as QuestionNav } from "./QuestionNav";
